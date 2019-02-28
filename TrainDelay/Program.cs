823fbbb24a44f7f0daeb90c8b8adc92a4cb55ce2
sumouchan <sdvx0613@gmail.com>
2019-03-01 00:04:42 +0900

    add TrainDelay

--- TrainDelay/Program.cs
+++ TrainDelay/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace TrainDelay
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tasks = File.ReadLines("routeNumbers.txt")
+                .Select(number => new TrainDelay(number).Run());
+
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception e)
+            {
+                new LineNotify().Send(e.ToString());
+            }
         }
     }
 }
---
+++ TrainDelay/TrainDelay.cs
@@ -0,0 +1,42 @@
+﻿using AngleSharp.Html.Parser;
+using AngleSharp.Dom;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace TrainDelay
+{
+    public class TrainDelay
+    {
+        private readonly string routeNumber;
+
+        public TrainDelay(string routeNumber)
+        {
+            this.routeNumber = routeNumber;
+        }
+
+        private IRestResponse GetHtml()
+        {
+            var client = new RestClient("https://transit.yahoo.co.jp/");
+            return client.Execute(new RestRequest($"traininfo/detail/{this.routeNumber}/"));
+        }
+
+        private bool IsDelayed(IElement element) =>
+            element.GetElementsByClassName("icnNormalLarge").Length == 0;
+
+        public async Task Run()
+        {
+            var response = GetHtml();
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(response.Content);
+            var statusElement = document.GetElementById("mdServiceStatus");
+
+            if (!IsDelayed(statusElement))
+                return;
+
+            var header = statusElement.QuerySelector("dt").LastChild.TextContent;
+            var message = statusElement.QuerySelector("p").TextContent;
+            var routeName = document.QuerySelector("h1").TextContent;
+            new LineNotify().Send($"\r\n【{routeName}】\r\n【{header}】\r\n{message}");
+        }
+    }
+}
