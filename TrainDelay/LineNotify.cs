d33bb97a5554785b27e22b8a06d24ea746737520
eiken7kyuu <11444818+eiken7kyuu@users.noreply.github.com>
2019-03-01 00:04:42 +0900

    add TrainDelay

---
+++ TrainDelay/LineNotify.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+
+namespace TrainDelay
+{
+    public class LineNotify
+    {
+        public void Send(string message)
+        {
+            var client = new RestClient("https://notify-api.line.me/api/notify/");
+            var request = new RestRequest();
+            request.Method = Method.POST;
+            request.AddParameter("message", message);
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddHeader("Authorization", $"Bearer {Environment.GetEnvironmentVariable("LINE_NOTIFY_TOKEN")}");
+            client.Execute(request);
+        }
+    }
+}
