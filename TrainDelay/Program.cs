592da12ebe749633a8da8f14ed049bbfad2e1581
eiken7kyuu <11444818+eiken7kyuu@users.noreply.github.com>
2020-06-19 01:59:04 +0900

    設定ファイルをテキストからjsonに変更

--- TrainDelay/Program.cs
+++ TrainDelay/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.IO;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace TrainDelay
 {
@@ -9,11 +10,11 @@ namespace TrainDelay
     {
         static async Task Main(string[] args)
         {
-            var tasks = File.ReadLines("routeNumbers.txt")
-                .Select(number => new TrainDelay(number).Run());
-
             try
             {
+                var jsonString = File.ReadAllText("config.json");
+                var numbers = JsonSerializer.Deserialize<Route>(jsonString).Numbers;
+                var tasks = numbers.Select(number => new TrainDelay(number).Run());
                 await Task.WhenAll(tasks);
             }
             catch (Exception e)
---
+++ TrainDelay/Route.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace TrainDelay
+{
+    class Route
+    {
+        public IEnumerable<string> Numbers { get; set; }
+    }
+}
